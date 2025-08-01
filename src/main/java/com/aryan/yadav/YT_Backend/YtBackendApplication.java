498199ef3778c5e8020507ea0daca01949581da7
Aryan Yadav <aryan225@code@gmail.com>
2025-08-01 13:44:11 +0530

    first commit

---
+++ src/main/java/com/aryan/yadav/YT_Backend/YtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aryan.yadav.YT_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtBackendApplication.class, args);
+	}
+
+}
