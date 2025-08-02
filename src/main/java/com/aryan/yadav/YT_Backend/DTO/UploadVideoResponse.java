f8a270315b434f2bd582e56975c7628a02d8ceb2
Aryan Yadav <aryan225@code@gmail.com>
2025-08-02 14:49:32 +0530

    Fixed the 406 error

--- src/main/java/com/aryan/yadav/YT_Backend/DTO/UploadVideoResponse.java
+++ src/main/java/com/aryan/yadav/YT_Backend/DTO/UploadVideoResponse.java
@@ -1,18 +1,39 @@
 package com.aryan.yadav.YT_Backend.DTO;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
 public class UploadVideoResponse {
 
     private String videoId;
     private String videoUrl;
 
+    public UploadVideoResponse() {
+    }
+
     public UploadVideoResponse(String videoId, String videoUrl) {
         this.videoId = videoId;
         this.videoUrl = videoUrl;
+    }
+
+    public String getVideoId() {
+        return videoId;
+    }
+
+    public void setVideoId(String videoId) {
+        this.videoId = videoId;
+    }
+
+    public String getVideoUrl() {
+        return videoUrl;
+    }
+
+    public void setVideoUrl(String videoUrl) {
+        this.videoUrl = videoUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "UploadVideoResponse{" +
+                "videoId='" + videoId + '\'' +
+                ", videoUrl='" + videoUrl + '\'' +
+                '}';
     }
 }
