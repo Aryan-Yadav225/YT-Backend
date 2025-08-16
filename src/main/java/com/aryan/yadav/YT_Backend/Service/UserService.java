142a52d3994289225ad8969f0a9a41a8976c500b
Aryan Yadav <aryan225code@gmail.com>
2025-08-16 22:48:01 +0530

    Added User Registration into DB

---
+++ src/main/java/com/aryan/yadav/YT_Backend/Service/UserService.java
@@ -0,0 +1,15 @@
+package com.aryan.yadav.YT_Backend.Service;
+
+import com.aryan.yadav.YT_Backend.Model.User;
+import com.aryan.yadav.YT_Backend.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+
+}
